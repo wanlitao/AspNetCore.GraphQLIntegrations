9caadecc94ad8d6e2932931a036b261e6fd0296e
万里涛 <wanlitao_3055601@outlook.com>
2020-02-17 22:44:43 +0800

    添加项目文件。

---
+++ README.md
@@ -0,0 +1,4 @@
+# AspNetCore.SecurityIntegrations
+[![License Apache](https://img.shields.io/badge/license-Apache%202-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+GraphQL Integrations Demo for AspNetCore WebApi
---
+++ src/AspNetCore.WebApi/Startup.cs
@@ -0,0 +1,46 @@
+using GraphQL.Server;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCore.WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private readonly IConfigurationSection _appSettings;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _appSettings = Configuration.GetSection("appSettings");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = true;
+                options.ExposeExceptions = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGraphQL<ISchema>();
+
+            app.UseGraphiQLServer();
+        }
+    }
+}
